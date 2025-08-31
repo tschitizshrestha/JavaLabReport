1833d7c6ab77c1b12c6262b82374c58b0d399353
Bibek Shrestha <135505175+tschitizshrestha@users.noreply.github.com>
2025-08-31 08:02:09 +0545

    Update PayrollSystem.java

--- src/main/java/com/lab/oop/PayrollSystem.java
+++ src/main/java/com/lab/oop/PayrollSystem.java
@@ -2,8 +2,8 @@ package com.lab.oop;
 
 public class PayrollSystem {
     public static void main(String[] args) {
-        FullTimeEmployee fte = new FullTimeEmployee(1, "Ram Thapa", 100000);
-        PartTimeEmployee pte = new PartTimeEmployee(2, "Sita Shrestha", 300, 155);
+        FullTimeEmployee fte = new FullTimeEmployee(1, "David", 30000);
+        PartTimeEmployee pte = new PartTimeEmployee(2, "Daniel", 70, 300);
 
         fte.calculateSalary();
         pte.calculateSalary();
