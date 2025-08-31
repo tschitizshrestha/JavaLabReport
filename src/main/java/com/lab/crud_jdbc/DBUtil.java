1758d95ef2b4ca1927ae4577d971b43ba9b586ef
Bibek Shrestha <135505175+tschitizshrestha@users.noreply.github.com>
2025-08-31 08:00:40 +0545

    Update DBUtil.java

--- src/main/java/com/lab/crud_jdbc/DBUtil.java
+++ src/main/java/com/lab/crud_jdbc/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/management";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "faithfulcrow778";
+    private static final String PASSWORD = "bibek12";
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
